dd94470df5601d1dc1a10ac53da885ff082e9bae
82038198 <82038198@MPB26475.correiosnet.int>
2014-01-28 10:25:08 -0300

    novo inicio

--- src/mc/MC.java
+++ src/mc/MC.java
@@ -1,73 +1,54 @@
 package mc;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MC {
 
-    private final int[][] action = {{0, 1}, {1, 0}, {1, 1}, {0, 2}, {2, 0}};
+    private static final int[][] ACT = {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}, {0, 2, 1}, {2, 0, 1}};
 
-    public int[][] moveR(int[] stt) {
-        int[][] sts = new int[5][2];
-        for (int ii = 0; ii < sts.length; ii++) {
-            int a = stt[8];
-            int b = stt[9];
-            int c = stt[10];
-            int d = stt[11];
-            int e = stt[12];
-            int f = stt[13];
-            int g = action[ii][0];
-            int h = action[ii][1];
-            int i = stt[8] - g;
-            int j = stt[9] - h;
-            int k = 0;
-            int l = 3 - i;
-            int m = 3 - j;
-            int n = 1;
-            sts[ii] = new int[]{a, b, c, d, e, f, g, h, i, j, k, l, m, n};
-        }
-        List<int[]> stList = new ArrayList<>();
-        for (int[] s : sts) {
-            if(s[8] >= s[9] && s[11] >= s[12]) {
-                stList.add(s);
+    public List<int[]> moveR(int[] sIn) {
+        List<int[]> sList = new ArrayList<>();
+        for (int i = 0; i < ACT.length; i++) {
+            int[] sTemp = new int[9];
+            sTemp[0] = sIn[0] - ACT[i][0];
+            sTemp[1] = sIn[1] - ACT[i][1];
+            sTemp[2] = sIn[2] - ACT[i][2];
+            sTemp[3] = sIn[3] + ACT[i][0];
+            sTemp[4] = sIn[4] + ACT[i][1];
+            sTemp[5] = sIn[5] + ACT[i][2];
+            sTemp[6] = ACT[i][0];
+            sTemp[7] = ACT[i][1];
+            sTemp[8] = ACT[i][2];
+            if (!Arrays.equals(new int[]{sIn[6], sIn[7], sIn[8]}, new int[]{ACT[i][0], ACT[i][1], ACT[i][2]})) {
+                if (sTemp[0] >= sTemp[1] || sTemp[0] == 0 && sTemp[3] >= sTemp[4] || sTemp[3] == 0) {
+                    sList.add(sTemp);
+                }
             }
         }
-        int[][] filtered = new int[stList.size()][14];
-        for (int i = 0; i < stList.size(); i++) {
-            filtered[i] = stList.get(i);
-        }
-        return filtered;
+        return sList;
     }
-    
-    public int[][] moveL(int[] stt) {
-        int[][] sts = new int[5][2];
-        for (int ii = 0; ii < sts.length; ii++) {
-            int a = stt[8];
-            int b = stt[9];
-            int c = stt[10];
-            int d = stt[11];
-            int e = stt[12];
-            int f = stt[13];
-            int g = action[ii][0];
-            int h = action[ii][1];
-            int i = stt[8] - action[ii][0];
-            int j = stt[9] - action[ii][1];
-            int k = 1;
-            int l = stt[11] + action[ii][0];
-            int m = stt[12] + action[ii][1];
-            int n = 0;
-            sts[ii] = new int[]{a, b, c, d, e, f, g, h, i, j, k, l, m, n};
-        }
-        List<int[]> stLeft = new ArrayList<>();
-        for (int[] s : sts) {
-            if(s[8] >= s[9] && s[11] >= s[12]) {
-                stLeft.add(s);
+
+    public List<int[]> moveL(int[] sIn) {
+        List<int[]> sList = new ArrayList<>();
+        for (int i = 0; i < ACT.length; i++) {
+            int[] sTemp = new int[9];
+            sTemp[0] = sIn[0] + ACT[i][0];
+            sTemp[1] = sIn[1] + ACT[i][1];
+            sTemp[2] = sIn[2] + ACT[i][2];
+            sTemp[3] = sIn[3] - ACT[i][0];
+            sTemp[4] = sIn[4] - ACT[i][1];
+            sTemp[5] = sIn[5] - ACT[i][2];
+            sTemp[6] = ACT[i][0];
+            sTemp[7] = ACT[i][1];
+            sTemp[8] = ACT[i][2];
+            if (!Arrays.equals(new int[]{sIn[6], sIn[7], sIn[8]}, new int[]{ACT[i][0], ACT[i][1], ACT[i][2]})) {
+                if (sTemp[0] >= sTemp[1] || sTemp[0] == 0 && sTemp[3] >= sTemp[4] || sTemp[3] == 0) {
+                    sList.add(sTemp);
+                }
             }
         }
-        int[][] filtered = new int[stLeft.size()][14];
-        for (int i = 0; i < stLeft.size(); i++) {
-            filtered[i] = stLeft.get(i);
-        }
-        return filtered;
+        return sList;
     }
 }
