d973f73dfa1d37ccc9ff491cfb19538373b01a69
82038198 <82038198@MPB26475.correiosnet.int>
2014-01-29 10:14:46 -0300

    o mais proximo do exito ate agora

--- src/mc/MC.java
+++ src/mc/MC.java
@@ -3,79 +3,116 @@ package mc;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MC {
 
     private static final int[][] ACT = {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}, {0, 2, 1}, {2, 0, 1}};
+    private static LinkedList<List<int[]>> lList = new LinkedList<>();
 
-    public List<int[]> moveR(List<int[]> sInList) {
+    public void search(List<int[]> sList) {
+        lList.addLast(removeDup(sList));
+        for (int[] s : lList.getLast()) {
+            System.out.println(s[0] + " " + s[1] + " " + s[2] + " " + s[3] + " " + s[4] + " " + s[5]);
+        }
+        System.out.println("////////////////////////////////////////////////////////////");
+        lList.addLast(removeDup(moveR(lList.getLast())));
+        if (isGoal(lList.getLast())) {
+            return;
+        }
+        for (int[] s : lList.getLast()) {
+            System.out.println(s[0] + " " + s[1] + " " + s[2] + " " + s[3] + " " + s[4] + " " + s[5]);
+        }
+        System.out.println("////////////////////////////////////////////////////////////");
+        List<int[]> ml = removeDup(moveL(removeDup(lList.getLast())));
+
+        for (int[] s : ml) {
+            System.out.println(s[0] + " " + s[1] + " " + s[2] + " " + s[3] + " " + s[4] + " " + s[5]);
+        }
+        search(ml);
+    }
+
+    private boolean isGoal(List<int[]> sList) {
+        for (int[] stt : sList) {
+            if (Arrays.equals(new int[]{stt[0], stt[1], stt[2]}, new int[]{0, 0, 0})) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<int[]> moveR(List<int[]> sInList) {
         List<int[]> sList = new ArrayList<>();
         for (int[] sIn : sInList) {
             for (int i = 0; i < ACT.length; i++) {
-                int[] sTemp = new int[9];
+                int[] sTemp = new int[6];
                 sTemp[0] = sIn[0] - ACT[i][0];
                 sTemp[1] = sIn[1] - ACT[i][1];
                 sTemp[2] = sIn[2] - ACT[i][2];
                 sTemp[3] = sIn[3] + ACT[i][0];
                 sTemp[4] = sIn[4] + ACT[i][1];
                 sTemp[5] = sIn[5] + ACT[i][2];
-                sTemp[6] = ACT[i][0];
-                sTemp[7] = ACT[i][1];
-                sTemp[8] = ACT[i][2];
                 if (sTemp[0] <= 3 && sTemp[0] >= 0 && sTemp[1] <= 3 && sTemp[1] >= 0 && sTemp[2] <= 1 && sTemp[2] >= 0) {
-                    if (!Arrays.equals(new int[]{sIn[6], sIn[7], sIn[8]}, new int[]{ACT[i][0], ACT[i][1], ACT[i][2]})) {
-                        if (sTemp[0] >= sTemp[1] || sTemp[0] == 0 && sTemp[3] >= sTemp[4] || sTemp[3] == 0) {
-                            sList.add(sTemp);
-                        }
+                    if (sTemp[0] >= sTemp[1] || sTemp[0] == 0 && sTemp[3] >= sTemp[4] || sTemp[3] == 0) {
+                        sList.add(sTemp);
                     }
+
                 }
             }
         }
-        return removeTwofold(sList);
-        //return sList;
+        return removeDup(sList);
     }
 
-    public List<int[]> moveL(List<int[]> sInList) {
+    private List<int[]> moveL(List<int[]> sInList) {
         List<int[]> sList = new ArrayList<>();
         for (int[] sIn : sInList) {
             for (int i = 0; i < ACT.length; i++) {
-                int[] sTemp = new int[9];
+                int[] sTemp = new int[6];
                 sTemp[0] = sIn[0] + ACT[i][0];
                 sTemp[1] = sIn[1] + ACT[i][1];
                 sTemp[2] = sIn[2] + ACT[i][2];
                 sTemp[3] = sIn[3] - ACT[i][0];
                 sTemp[4] = sIn[4] - ACT[i][1];
                 sTemp[5] = sIn[5] - ACT[i][2];
-                sTemp[6] = ACT[i][0];
-                sTemp[7] = ACT[i][1];
-                sTemp[8] = ACT[i][2];
                 if (sTemp[0] <= 3 && sTemp[0] >= 0 && sTemp[1] <= 3 && sTemp[1] >= 0 && sTemp[2] <= 1 && sTemp[2] >= 0) {
-                    if (!Arrays.equals(new int[]{sIn[6], sIn[7], sIn[8]}, new int[]{ACT[i][0], ACT[i][1], ACT[i][2]})) {
-                        if (sTemp[0] >= sTemp[1] || sTemp[0] == 0 && sTemp[3] >= sTemp[4] || sTemp[3] == 0) {
-                            sList.add(sTemp);
-                        }
+                    if (sTemp[0] >= sTemp[1] || sTemp[0] == 0 && sTemp[3] >= sTemp[4] || sTemp[3] == 0) {
+                        sList.add(sTemp);
                     }
+
                 }
             }
         }
-        return removeTwofold(sList);
-        //return sList;
+        return removeDup(sList);
     }
 
-    private List<int[]> removeTwofold(List<int[]> list) {
-        int count = 0;
-        List<int[]> filtered = new ArrayList<>();
-        for (int ii = 0; ii < list.size(); ii++) {
-            for (int i = 0; i < list.size(); i++) {
-                if (Arrays.equals(new int[]{list.get(i)[0], list.get(i)[1], list.get(i)[2]}, new int[]{list.get(ii)[0], list.get(ii)[1], list.get(ii)[2]})) {
-                    count++;
+    private List<int[]> removeDup(List<int[]> list) {
+        HashSet<int[]> hs = new HashSet<>();
+        hs.addAll(list);
+        list.clear();
+        list.addAll(hs);
+        return list;
+    }
+
+    private List<int[]> removeParent(List<int[]> grandpa, List<int[]> parent, List<int[]> child) {
+        List<int[]> dup = new ArrayList<>();
+        for (int[] p : parent) {
+            for (int[] c : child) {
+                if (Arrays.equals(c, p)) {
+                    dup.add(c);
                 }
             }
-            if (count == 2) {
-                filtered.add(list.get(ii));
-            }
-            count = 0;
         }
-        return filtered;
+        for (int[] pa : grandpa) {
+            for (int[] c : child) {
+                if (Arrays.equals(c, pa)) {
+                    dup.add(c);
+                }
+            }
+        }
+        for (int[] d : dup) {
+            child.remove(d);
+        }
+        return child;
     }
 }
